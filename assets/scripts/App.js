10ce38914085789420325f28aacbdda47580d83a
Theo <72858516+Blyss09@users.noreply.github.com>
2024-05-02 13:50:48 +0200

    Screens + avancement html CSS

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -18,19 +18,8 @@ export default class App {
 
     }
 
-    hanglePageClick = (e) => {
-        const link = e.target.closest('[target="spa"]');
-        if (link) {
-            e.preventDefault();
-            history.pushState(null, null, link.href);
-            this.navigate();
-        }
-    }
-
-    navigate = (e) => {
+    render(){
         document.getElementById('app').firstElementChild?.remove();
-        const currentPage = document.createElement('div');
-        currentPage.innerText = location.pathname;
-        document.getElementById('app').append(currentPage);
+        document.getElementById('app').append(this.router.screen);
     }
 }
---
+++ assets/scripts/Router.js
@@ -0,0 +1,48 @@
+import AboutScreen from "../screens/AboutScreen.js";
+import BookingScreen from "../screens/BookingScreen.js";
+import MenuScreen from "../screens/MenuScreen.js";
+import SamediScreen from "../screens/SamediScreen.js";
+import App from "./App.js";
+import HomeScreen from "../screens/HomeScreen.js";
+import LoginScreen from "../screens/LoginScreen.js";
+import NotfoundScreen from "../screens/NotfoundScreen.js";
+
+export default class Router{
+    #screen = null;
+    get screen() {return this.#screen;}
+
+    constructor(){
+
+        window.onclick = this.handlePageClick;
+        window.onload = this.navigate;
+        window.onpopstate = this.navigate;
+    }
+
+    handlePageClick = (e) => {
+        const link = e.target.closest('[target="spa"]');
+        if(link){
+            e.preventDefault();
+            history.pushState(null, null, link.href);
+            this.navigate();
+        }
+    }
+
+    navigate = (e) => {
+        if (location.pathname == "/" || location.pathname == "/home") {
+          this.#screen = new HomeScreen();
+        } else if (location.pathname == "/login") {
+          this.#screen = new LoginScreen();
+        } else if (location.pathname == "/menu") {
+          this.#screen = new MenuScreen();
+        } else if (location.pathname == "/samedi") {
+          this.#screen = new SamediScreen();
+        } else if (location.pathname == "/booking") {
+          this.#screen = new BookingScreen();
+        } else if (location.pathname == "/about-us") {
+          this.#screen = new AboutScreen();
+        } else {
+          this.#screen = new NotfoundScreen();
+        }
+        App.instance.render();
+      };
+}
