c94362a3a894238611d8d5e6f6aab0f7de143fc7
Solen <160152102+Naxxale@users.noreply.github.com>
2024-05-06 09:45:24 +0200

    Ajout footer

--- assets/screens/MenuScreen.js
+++ assets/screens/MenuScreen.js
@@ -89,12 +89,24 @@ export default class MenuScreen extends HTMLElement{
     </div>
   </div>
         <!--Footer-->
-        <div class="container-fluid bg-secondary d-flex justify-content-around">
-          <p style ="color:white">Termes et conditions</p>
-          <p style="color:white">Mentions légales</p>
-          <p style="color:white"> <i class="bi bi-c-circle"></i> 2024 par SAT'URDAY</p>
+        <footer class="py-5 mt-5">
+        <hr />
+        <div class="container text-light text-center">
+          <p class="display-5 mb-3 fw-bold text-secondary">Suivez nous sur les réseaux !</p>
+          <div class="container mb-3 icon-area">
+            <a href="https://www.facebook.com/" class="social-logo m-3"
+              ><i class="bi bi-facebook h1"></i
+            ></a>
+            <a href="https://www.tiktok.com/" class="social-logo m-3"
+              ><i class="bi bi-tiktok h1"></i
+            ></a>
+            <a href="https://www.instagram.com/" class="social-logo m-3"
+              ><i class="bi bi-instagram h1"></i
+            ></a>
+          </div>
+          <small class="text-secondary">made with love by Antoine C</small>
         </div>
-      </div>`
+      </footer>`
     }
 }
 
